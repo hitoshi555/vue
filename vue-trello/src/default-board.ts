95d5d5d901d45c5cd2bea648f27c225ebe4c291b
hitoshi555 <hitoshi1845@gmail.com>
2024-05-13 21:00:28 +0900

    add projects

---
+++ vue-trello/src/default-board.ts
@@ -0,0 +1,79 @@
+type Task = {
+  description: string
+  name: string
+  id: string
+  userAssigned: string | null
+}
+
+type Column = {
+  name: string
+  tasks: Task[]
+}
+
+const workshop: {
+  name: string
+  columns: Column[]
+} = {
+  name: 'workshop',
+  columns: [
+    {
+      name: 'todo',
+      tasks: [
+        {
+          description: 'first task details',
+          name: 'The first task',
+          id: '12a34er',
+          userAssigned: null
+        },
+        {
+          description: '',
+          name: 'Second todo task',
+          id: '12a34et',
+          userAssigned: null
+        },
+        {
+          description: 'Get ready to drag&drop columns and tasks',
+          name: 'Get ready',
+          id: '12a34ey',
+          userAssigned: null
+        }
+      ]
+    },
+    {
+      name: 'in-progress',
+      tasks: [
+        {
+          description: 'This is description for in progress task',
+          name: 'first in-progress task',
+          id: '12a34eu',
+          userAssigned: null
+        },
+        {
+          description: '',
+          name: 'Another task in progress',
+          id: '12a34ea',
+          userAssigned: null
+        }
+      ]
+    },
+    {
+      name: 'done',
+      tasks: [
+        {
+          description: 'Ready to be checked task in done column',
+          name: 'Done task',
+          id: '12a34ei',
+          userAssigned: null
+        },
+        {
+          description: 'Done properly',
+          name: 'Best task ever',
+          id: '12a34es',
+          userAssigned: null
+        }
+      ]
+    }
+  ]
+}
+
+export default workshop
